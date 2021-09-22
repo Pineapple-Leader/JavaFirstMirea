bd01a90a2a0c2a14d159de91e30fbff82e61fe88
Pineapple-Leader <y.grigoriy@inbox.ru>
2021-09-22 17:45:28 +0300

    Lab 1 Task 3

---
+++ src/ru/mirea/task1/opt3/third.java
@@ -0,0 +1,11 @@
+package ru.mirea.task1.opt3;
+
+public class third {public static void main(String[] args) {
+    int i=1;
+    while (i<=10) {
+        String str = String.format("%.2f",1.0/i);
+        System.out.println("аргумент №" + i + " = " + str);
+        i++;
+    }
+}
+}
