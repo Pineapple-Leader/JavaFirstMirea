36a4a6a0060b15bbe3bc4fba3948e3cfe08bc80f
Pineapple-Leader <y.grigoriy@inbox.ru>
2021-09-22 18:00:52 +0300

    Lab 1 Task 2

--- src/ru/mirea/task1/opt2/second.java
+++ src/ru/mirea/task1/opt2/second.java
@@ -2,11 +2,8 @@ package ru.mirea.task1.opt2;
 
 public class second {
     public static void main(String[] args) {
-        int i=1;
-        while (i<=10) {
-            String str = String.format("%.2f",1.0/i);
-            System.out.println("аргумент №" + i + " = " + str);
-            i++;
+        for (int a = 0; a < 10; a++) {
+            System.out.println(1./a);
         }
     }
 }
