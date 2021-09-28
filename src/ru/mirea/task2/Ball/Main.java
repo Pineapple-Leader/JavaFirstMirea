b4028999ad6cc7e2954000c7dae8b0b3c59bb010
Pineapple-Leader <12112002ohahac>
2021-09-28 18:53:54 +0300

    task 2 Ball

--- src/ru/mirea/task2/Ball/Main.java
+++ src/ru/mirea/task2/Ball/Main.java
@@ -3,9 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         Ball footballBall = new Ball("Football", 430.0f, 70.0f);
-        Ball basketballBall = new Ball("basketball", 600f, 600f);
-        Ball tennisBall = new Ball("tennis", 2.7f, 6.7f);
-        basketballBall.setWeight(3);
+        Ball basketballBall = new Ball("basketball", 3f, 600f);
+        Ball tennisBall = new Ball("tennis", 2.7f, 1f);
+        basketballBall.setWeight(600f);
+        tennisBall.setDiameter(6.7f);
         footballBall.print();
         basketballBall.print();
         tennisBall.print();
