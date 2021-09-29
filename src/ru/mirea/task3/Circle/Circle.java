8536cd786b82d4f72cc88a189955c94ef8f589a2
Pineapple-Leader <12112002ohahac>
2021-09-29 08:41:31 +0300

    Task 3 Circle

--- src/ru/mirea/task3/Circle/Circle.java
+++ src/ru/mirea/task3/Circle/Circle.java
@@ -19,4 +19,16 @@ package ru.mirea.task3.Circle;
     public  void setRadius(int radius){
         this.radius=radius;
     }
+     public int getX(){
+         return x;
+     }
+     public  void setX(int x){
+         this.x=x;
+     }
+     public int getY(){
+         return y;
+     }
+     public  void setY(int y){
+         this.y=y;
+     }
 }
