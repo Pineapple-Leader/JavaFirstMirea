f36332a4cd279c5ffa8718a5ba5b8097b1e92976
Pineapple-Leader <12112002ohahac>
2021-11-08 09:05:00 +0300

    Task 8

---
+++ src/ru/mirea/task8/Animation/animation.java
@@ -0,0 +1,40 @@
+package ru.mirea.task8.Animation;
+
+
+
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+public class animation extends JFrame{
+
+    private JLabel gifLabel;
+    private Container controls;
+
+    public animation(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        this.setSize(400, 400);
+        this.startWindow();
+    }
+
+    void startWindow(){
+        controls = new Container();
+        controls = getContentPane();
+        controls.setLayout(new FlowLayout());
+        gifLabel = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().createImage("C:\\Users\\1\\Desktop\\Cat.gif\\")));
+        controls.add(gifLabel);
+
+    }
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                animation gif = new animation();
+                gif.setVisible(true);
+            }
+        });
+    }
+}
